e3d431445102798ce21e505f48487f75fd1e5f0c
Carlos Betancourt Carrero <carbetacar@gmail.com>
2018-04-29 16:20:20 -0300

    Log process.env.FEATHERS_HOST

--- plugins/feathers.js
+++ plugins/feathers.js
@@ -4,6 +4,7 @@ import Feathers from '@feathersjs/client';
 import socketio from '@feathersjs/socketio-client';
 import io from 'socket.io-client';
 
+console.log('process.env.FEATHERS_HOST', process.env.FEATHERS_HOST);
 const host = process.env.FEATHERS_HOST || 'http://localhost:3000';
 const socket = io(host, {
   transports: ['websocket'],
